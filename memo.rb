a3ad20b0bcfc65ebe3c0bf6f576f0cd2ddbd6f8a
EC2 Default User <ec2-user@ip-172-31-26-225.ec2.internal>
2022-05-20 18:15:28 +0000

    初回

---
+++ memo.rb
@@ -0,0 +1,43 @@
+ require "csv"
+ 
+    puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+ 
+    memo_type = gets.to_i
+    
+ 
+ if memo_type == 1
+   
+   
+   puts "新規でメモを記載します。拡張子を除いたファイル名を入力して下さい。"
+   file_name = gets.chomp
+   
+   puts "メモ内容を記載してください。Ctrl+Dで保存します。"
+   memo = STDIN.read
+   
+   CSV.open("#{file_name}.cvs", "w") do |csv|
+   csv.puts["#{memo}"]
+   
+ end
+ 
+ elsif
+     memo_type == 2
+     
+     puts "既存のメモを編集します。各調子を除いた既存のファイル名を入力して下さい。"
+     file_name = gets.champ
+     
+     puts "編集したい内容を入力して下さい。完了したらCtrl+Dを押して下さい"
+     memo = STDIN.read
+     
+     CSV.open("#{file_name}.csv", "a") do |csv|
+     csv.puts["#{memo}"]
+  end
+     
+ else
+    puts "1か2を入力して下さい。"
+ end
+   
+ 
+ 
+
+
+  
